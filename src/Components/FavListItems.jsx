b7fb745dd8aa47cfb598583ded34336e0fb4b836
MohammadSalhab99 <mohammadsalhab8@gmail.com>
2022-02-16 21:34:42 +0200

    solved

---
+++ src/Components/FavListItems.jsx
@@ -0,0 +1,30 @@
+import { Button, Card, CardGroup } from "react-bootstrap";
+
+function FavListItems(props){
+    return(
+        <>
+        <CardGroup style={{ display: "flex" }}>
+            {
+                
+                (props.favoriteList ?? []).map(movie => {
+                    return (
+                        <Card key={movie.idDrink}>
+                            <Card.Img variant="top" src={meme.strDrinkThumb} />
+                            <Card.Body>
+                                <Card.Title>{movie.title}</Card.Title>
+                                <Card.Text>
+                                    {movie.caption}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    )
+                })
+            }
+            
+        
+        </CardGroup>
+        </>
+    )
+}
+
+export default FavListItems;
